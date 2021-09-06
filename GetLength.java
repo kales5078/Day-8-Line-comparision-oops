0e280dfd98adb2fbf99dd8f0d4fd46bf0fd45d01
Suraj Kale <88039478+kales5078@users.noreply.github.com>
2021-09-06 08:19:49 +0530

    Add : Initial Commit

---
+++ GetLength.java
@@ -0,0 +1,35 @@
+package geometry;
+
+import java.util.Scanner;
+
+public class GetLength {
+
+	public static void main(String arg[]) {
+
+		// Declaration Part
+		int x1, x2, y1, y2;
+		double Length;
+
+		// Get the input x1,y1,x2,y2 from the user
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("enter x1 point");
+		x1 = sc.nextInt();
+
+		System.out.println("enter y1 point");
+		y1 = sc.nextInt();
+
+		System.out.println("enter x2point");
+		x2 = sc.nextInt();
+
+		System.out.println("enter y2 point");
+		y2 = sc.nextInt();
+		
+		 // calculate the Length of the Line
+		Length = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+		System.out.println("Length between " + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ") =" + Length);
+
+	}
+
+}
