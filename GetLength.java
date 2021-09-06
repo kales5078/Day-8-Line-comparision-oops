91d2620a97debba21ef5952d451b1feac712936e
Suraj Kale <88039478+kales5078@users.noreply.github.com>
2021-09-06 08:53:05 +0530

    Add : Initial commit

--- GetLength.java
+++ GetLength.java
@@ -1,14 +1,31 @@
 package geometry;
 
+// Declaration Part
+		static int x1, x2, y1, y2;
+		double Length;
+
 import java.util.Scanner;
+
 
 public class GetLength {
 
+	
+	
+// Declaration Part
+		static int x1, x2, y1, y2;
+		double Length;
+	
+	//method for calculate length
+public void calculateLength(){ 
+ // calculate the Length of the Line
+		Length = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+		System.out.println("Length between " + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ") =" + Length);
+}
+
 	public static void main(String arg[]) {
 
-		// Declaration Part
-		int x1, x2, y1, y2;
-		double Length;
+		
 
 		// Get the input x1,y1,x2,y2 from the user
 		Scanner sc = new Scanner(System.in);
@@ -25,11 +42,9 @@ public class GetLength {
 		System.out.println("enter y2 point");
 		y2 = sc.nextInt();
 		
-		 // calculate the Length of the Line
-		Length = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-
-		System.out.println("Length between " + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ") =" + Length);
-
+       		GetLength obj = new GetLength();
+	         obj.calculateLength();	
+                     
 	}
 
 }
